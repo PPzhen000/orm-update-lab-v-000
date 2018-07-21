aef09d5b20cc1e0222cffaa8cef5c88d1c640d21
Ariel Zhen <zhenpingping@live.com>
2018-07-21 07:52:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,11 +21,12 @@ class Student
         grade TEXT
       )
     SQL
-    DB[:conn].execute(sql) 
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
-
+    sql = "DROP TABLE students"
+    DB[:conn].execute(sql) 
   end
 
   def save
