4de3d962ada145119fea8c75928c606650127323
Ariel Zhen <zhenpingping@live.com>
2018-07-21 08:11:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,8 +44,8 @@ class Student
 
   def self.create(name:, grade:)
     student = Student.new(name, grade)
-    # student.save
-    # student
+    student.save
+    student
   end
 
   def self.new_from_db(row)
